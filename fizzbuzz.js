ce28fb80168ae1172f286e944f3731927f8fb43f
maddrake220 <jwchoi4118@gmail.com>
2021-11-25 13:37:37 +0900

    fix: algorithm changes

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,9 @@
 for (let i = 1; i <= 100; i++) {
-	i % 3 ? (i % 5 ? console.log("fizzbuzz") : console.log("fizz")) : i % 5 ? console.log("buzz") : console.log(i)
+  i % 3 == 0
+    ? i % 5 == 0
+      ? console.log("fizzbuzz")
+      : console.log("fizz")
+    : i % 5 == 0
+    ? console.log("buzz")
+    : console.log(i);
 }
